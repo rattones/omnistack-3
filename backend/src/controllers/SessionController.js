0ef986edc8411efe2356b45e8592fbb1cffe89e3
Marcelo Ratton <rattones@marceloratton.com>
2020-03-27 01:10:37 -0300

    fixing

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -21,7 +21,7 @@ module.exports = {
 
         const ong= await connection('ongs')
             .where('email', email)
-            .select('name, id')
+            .select(['name', 'id'])
             .first();
 
         if (!ong) {
